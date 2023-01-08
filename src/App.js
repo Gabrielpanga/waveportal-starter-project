200632e90496671b6b0f606ef769b945da15a2cd
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2023-01-08 17:52:44 +0100

    feat: interact with goerli deployed contract

--- src/App.js
+++ src/App.js
@@ -1,29 +1,165 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
-import './App.css';
+import "./App.css";
+import abi from "./utils/WavePortal.json";
+
+const getEthereumObject = () => window.ethereum;
+
+/*
+ * This function returns the first linked account found.
+ * If there is no account linked, it will return null.
+ */
+const findMetaMaskAccount = async () => {
+  try {
+    const ethereum = getEthereumObject();
+
+    /*
+     * First make sure we have access to the Ethereum object.
+     */
+    if (!ethereum) {
+      console.error("Make sure you have Metamask!");
+      return null;
+    }
+
+    console.log("We have the Ethereum object", ethereum);
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      return account;
+    } else {
+      console.error("No authorized account found");
+      return null;
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 
 export default function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [totalWaves, setTotalWaves] = useState("");
 
-  const wave = () => {
-    
-  }
-  
+  const contractAddress = "0xD6F50750F73fDaE81c109C124704f36361828710";
+  const contractABI = abi.abi;
+
+  const getTotalWaves = async () => {
+    try {
+      const ethereum = getEthereumObject();
+
+      /*
+       * First make sure we have access to the Ethereum object.
+       */
+      if (!ethereum) {
+        console.error("Make sure you have Metamask!");
+        return;
+      }
+
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const wavePortalContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      let count = await wavePortalContract.getTotalWaves();
+      setTotalWaves(count.toNumber());
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    findMetaMaskAccount().then((account) => {
+      if (account !== null) {
+        setCurrentAccount(account);
+        getTotalWaves();
+      }
+    });
+  });
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Ethereum object doesn't exist!");
+        return;
+      }
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const wavePortalContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+
+      let count = await wavePortalContract.getTotalWaves();
+      console.log("Retrieved total wave count...", count.toNumber());
+
+      const waveTxn = await wavePortalContract.wave();
+      console.log("Mining...", waveTxn.hash);
+
+      await waveTxn.wait();
+      console.log("Mined -- ", waveTxn.hash);
+
+      count = await wavePortalContract.getTotalWaves();
+      console.log("Retrieved total wave count...", count.toNumber());
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const ethereum = getEthereumObject();
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
-        <div className="header">
-        👋 Que miras bobo?
+        <div className="header">👋 Que miras bobo? Anda palla </div>
+
+        <div className="bio">
+          Hey I'm learning to build smart contracts. Connect your Ethereum
+          wallet and wave at me! (This is currently working only on "Goerli
+          Network")
         </div>
 
         <div className="bio">
-          Hey I'm learning to build smart contracts.
-          Connect your Ethereum wallet and wave at me!
+          I received <strong>{totalWaves} wave(s)</strong> already!
         </div>
 
         <button className="waveButton" onClick={wave}>
-          Hi!
+          Holu!
         </button>
+
+        {/*
+         * If there is no currentAccount render this button
+         */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </div>
   );
